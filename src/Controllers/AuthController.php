ecd27572a58fbcf6ca4dbf9d630f3fcf31ef983b
Gabriel <eu@gabrielf.com>
2018-11-20 17:04:02 -0200

    The storage permission of profile pictures should be public

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -116,7 +116,7 @@ class AuthController extends Controller
 
         $form->display('username', trans('admin.username'));
         $form->text('name', trans('admin.name'))->rules('required');
-        $form->image('avatar', trans('admin.avatar'));
+        $form->image('avatar', trans('admin.avatar'))->storage_permission('public');
         $form->password('password', trans('admin.password'))->rules('confirmed|required');
         $form->password('password_confirmation', trans('admin.password_confirmation'))->rules('required')
             ->default(function ($form) {
